72c14d526faba08cb270379930746dd6ccfc2396
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2023-10-26 18:42:35 +0900

    1026

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     env: {
         MONGODB_URL: "mongodb+srv://lockheart:RAZz8rsNILo88Smq@cluster0.qs9wtoh.mongodb.net/mybook?retryWrites=true&w=majority",
-        HOMEPAGE_URL: "http://localhost:3000/"
+        HOMEPAGE_URL: "nextjs-project-lockhearts-projects.vercel.app"
     },
 
 }
